19f5bad9bfdadfa6d9e129e89a211609b5750448
allysonaggp <allysonaggp@gmail.com>
2025-06-27 21:12:29 -0300

    update

--- js/api.js
+++ js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/allysonaggp/Portifolio/refs/heads/main/data/profile.json';
     const fetching = await fetch(url)
     const profileData = await fetching.json()
     return profileData
